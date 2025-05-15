c0bcc55b6bd5fd34a0e3d10315a9e33369f64a86
LilianDamico <lfonseca_damico@hotmail.com>
2025-05-15 20:29:22 -0300

    new3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
-export const appConfig = {
-  providers: [provideRouter(routes)],
-};
+export const appConfig = [
+  provideRouter(routes, withHashLocation())
+];
