6e06950542a907272c14bdfea1917f92c251c85f
LilianDamico <lfonseca_damico@hotmail.com>
2025-05-15 19:56:08 -0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeadComponent } from '../app/modules/principal/components/head/head.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadComponent],
+  imports: [RouterOutlet],
   template: `
-    <app-head />
     <router-outlet />
   `
 })
